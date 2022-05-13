a09b1709d40646b896af46d40d9e37baa53e562a
Vinny Barone <vinnymbarone@gmail.com>
2022-05-12 21:03:44 -0400

    second commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "contents";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    this.classList.remove('active');
+  }
+
+
+
